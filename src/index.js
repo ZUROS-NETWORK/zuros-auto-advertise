55c3fefc13c031711a4fe155310f75797e395ac5
LuciaNishimiya <lucianishimiya@gmail.com>
2023-09-30 23:01:29 -0400

    Add error handling and automatic retries

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const { BotAdminToken, LogChannelId, CommandChannelId } = require('./config.json');
-
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({
     intents: [
@@ -11,38 +10,52 @@ const client = new Client({
     ],
 });
 const { RunBots, stopBot, RunningBots, BotList } = require('./services/botmanager');
-client.once(Events.ClientReady, c => {
-    console.log(`El bot ${c.user.tag} se ha reiniciado.`);
-    const logs = client.channels.cache.get(LogChannelId)
-    logs.send('# :warning:  El bot se ha reiniciado.')
-    RunBots({ logs, BotList });
 
+client.once(Events.ClientReady, async (c) => {
+    try {
+        console.log(`El bot ${c.user.tag} se ha reiniciado.`);
+        const logs = client.channels.cache.get(LogChannelId);
+        logs.send('# :warning:  El bot se ha reiniciado.');
+        await RunBots({ logs, BotList });
+    } catch (error) {
+        console.error('Error al reiniciar el bot:', error);
+        const logs = client.channels.cache.get(LogChannelId);
+        logs.send('# :warning:Error al reiniciar el bot:', error);
+        await RunBots({ logs, BotList });
+    }
 });
+
 client.on('messageCreate', async (msg) => {
-    const CommandChannel = client.channels.cache.get(CommandChannelId)
-    const args = msg.content.split(' ');
-    const command = args[0].toLowerCase()
-    const botName = args.slice(1).join(' ');
+    try {
+        const CommandChannel = client.channels.cache.get(CommandChannelId);
+        if (!CommandChannel) {
+            console.error('No se pudo encontrar el canal de comandos (CommandChannelId)');
+            return;
+        }
+        const args = msg.content.split(' ');
+        const command = args[0].toLowerCase();
+        const botName = args.slice(1).join(' ');
 
-    if (msg.author.bot) return;
-    if (!CommandChannel) return;
-    if (command == 'stop') {
-        msg.reply(await stopBot(botName))
-
+        if (msg.author.bot) return;
+        if (command == 'stop') {
+            msg.reply(await stopBot(botName));
+        }
+        if (command == 'start') {
+            // SUS
+        }
+        if (command == 'list') {
+            let list = '>>> ##  Lista de bots:\n';
+            BotList.forEach((bot) => {
+                list += `- **${bot}**\n`;
+            });
+            msg.reply(list);
+        }
+    } catch (error) {
+        console.error('Error al procesar un mensaje:', error);
     }
-    if (command == 'start') {
-
-    }
-
-    if (command == 'list') {
-        let lsit = '>>> ##  Lista de bots:\n';
-        BotList.forEach(bot => {
-            lsit += `- **${bot}**\n`;
-        });
-        msg.reply(lsit)
-    }
-
-
 });
 
-client.login(BotAdminToken);
+client.login(BotAdminToken)
+    .catch(error => {
+        console.error('Error al iniciar sesión del bot:', error);
+    });
